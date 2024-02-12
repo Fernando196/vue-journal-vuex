444ef2a69e7605b8aff1efbd3a7566da1cb9fdb8
unknown <fm@datalabmx.com>
2024-02-12 17:13:17 -0600

    ini commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name:'entry', params:{ id: 10 } })"
+  >
+
+    <!-- Titulo -->
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">15</span>
+        <span class="mx-1 fs-5">Febrero</span>
+        <span class="mx-2 fs-5 fw-light">2024, lunes</span>
+    </div>
+
+    <div class="entry-description">
+        Duis cillum do ex aute culpa exercitation sunt sint velit laborum aute incididunt. Mollit non esse mollit eu veniam ad. Ipsum aliqua eiusmod dolore adipisicing commodo culpa nulla ut.
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'EntryItem'
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container{
+    border-bottom: 1px solid #3c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover{
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+    .entry-description{
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Est dolor est cupidatat ut ea. Labore sint tempor esse excepteur ad cillum. Ullamco id Lorem ipsum nostrud fugiat laborum eiusmod.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Veniam culpa cupidatat cillum aute elit velit enim duis irure minim aliquip eiusmod. Aliqua excepteur aliquip minim eu enim deserunt consectetur. Consequat sit dolor laboris ullamco sunt culpa sunt ullamco ad ad. Amet Lorem ipsum excepteur enim proident ullamco quis exercitation qui ut. Culpa eiusmod consequat et excepteur incididunt pariatur excepteur commodo pariatur. Esse do ea ex sit occaecat occaecat.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Amet Lorem est ex sint id id. Enim Lorem deserunt culpa ut incididunt fugiat qui officia irure et. Aute dolor ut ut ea culpa dolor. Aliqua consequat sint aliqua quis sint deserunt adipisicing qui pariatur do irure Lorem incididunt. Exercitation consectetur occaecat non est anim minim magna laborum amet voluptate exercitation.',
+            picture: null,
+        },
+    ]
+});
